35bcd70ed9c072928e6da5e51373e0dbdf5e9acd
samm1735 <ramclief17@gmail.com>
2024-03-26 12:55:58 -0400

    All the required tasks are done : - Added a ‘+’ button that takes the user to new ‘Add Card Screen’ - Added a Cancel button to the 'Add Card Screen' - Added two fields to the 'Add Card Screen' that will allow the user to enter the Question and the Answer - Added a Save button that will dismiss 'Add Card Screen' and return to the Main Activity where the user can see the card they just created

--- app/src/main/java/com/example/app2/AddCardActivity.kt
+++ app/src/main/java/com/example/app2/AddCardActivity.kt
@@ -1,11 +1,58 @@
 package com.example.app2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
+
 
 class AddCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_card)
+
+
+//        Buttons
+        val cancel_card_button = findViewById<View>(R.id.cancel_card_button)
+        val save_card_button = findViewById<View>(R.id.save_card_button)
+
+//        Inputs
+
+        val add_card_answer1_field = findViewById<EditText>(R.id.add_card_answer1_field)
+        val add_card_question_field = findViewById<EditText>(R.id.add_card_question_field)
+
+
+
+        cancel_card_button.setOnClickListener{
+            val cancelIntent = Intent(this, MainActivity::class.java)
+            startActivity(cancelIntent)
+            finish()
+        }
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            val data: Intent? = result.data
+        }
+
+        save_card_button.setOnClickListener{
+//            val saveCardIntent = Intent(this, MainActivity::class.java)
+
+            val saveCardIntent = Intent()
+            val s = add_card_question_field.text.toString()
+            val p = add_card_answer1_field.text.toString()
+
+            saveCardIntent.putExtra("new_card_question", add_card_question_field.text.toString())
+            saveCardIntent.putExtra("new_card_answer_1", add_card_answer1_field.text.toString())
+
+//            resultLauncher.launch(saveCardIntent)
+
+            setResult(RESULT_OK, saveCardIntent)
+
+            finish()
+
+        }
+
+
     }
 }
